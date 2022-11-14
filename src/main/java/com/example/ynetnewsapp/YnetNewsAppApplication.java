df4f71b465958f08724cb709370b09b9b04326fc
dima nakhleh <dimanakhleh@example.com>
2022-11-14 15:51:56 +0200

    adding my files

---
+++ src/main/java/com/example/ynetnewsapp/YnetNewsAppApplication.java
@@ -0,0 +1,14 @@
+package com.example.ynetnewsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YnetNewsAppApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(YnetNewsAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ynetnewsapp/YnetNewsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ynetnewsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YnetNewsAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
